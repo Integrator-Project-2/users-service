7845f0b01d821397e75382e7d807383f81b84954
Valéria <vvqf2016@gmail.com>
2024-03-21 14:29:33 -0300

    created models

--- pacients/models.py
+++ pacients/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Pacient(models.Model):
+    name = models.CharField(max_length=255)
+    cpf = models.CharField(max_length=11)
+    email = models.EmailField(unique=True)
+    password = models.CharField()
+    phone = models.CharField(max_length=11)
+    adress = models.CharField()
+    city = models.CharField()
+    state = models.CharField(max_length=2)
+    birth_date = models.DateField()
+    
+    def __str__(self):
+        return self.nome
+
+
