e718075791e2f9acf1baddcb0a59937b727baf0d
KarlaJulyana <karla@solarz.com.br>
2024-04-24 12:01:26 -0300

    :wrench: allow unauthenticated doctor make registration

--- doctors/views.py
+++ doctors/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, permissions
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.views import APIView
 from .models import Doctor
 from .serializers import DoctorSerializer
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+class CustomDoctorPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            return True
+        return request.user and request.user.is_authenticated
 
 class DoctorViewSet(viewsets.ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
-    permission_classes = []
+    permission_classes = [CustomDoctorPermission]
     
     def partial_update(self, request, *args, **kwargs):
         return Response({'message': 'PATCH method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
