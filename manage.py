b407b6c66ff7df0c346b580de08e022a3913b44c
KarlaJulyana <karla@solarz.com.br>
2024-05-06 16:54:05 -0300

    :wrench: change project default port to 8001

--- manage.py
+++ manage.py
@@ -15,6 +15,20 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    
+    # Check if 'runserver' command is being executed
+    if 'runserver' in sys.argv:
+        from django.conf import settings
+        # Get the PORT value from settings.py or default to 8000
+        port = getattr(settings, 'PORT', 8000)
+        # If 'runserver' is in sys.argv and there's at least one argument after it
+        if 'runserver' in sys.argv and sys.argv.index('runserver') + 1 < len(sys.argv):
+            # Replace the port value in sys.argv
+            sys.argv[sys.argv.index('runserver') + 1] = str(port)
+        else:
+            # Append the port argument to sys.argv
+            sys.argv.append(str(port))
+    
     execute_from_command_line(sys.argv)
 
 
