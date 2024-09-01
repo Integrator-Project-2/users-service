f56c4ab1ec71a60a6506459d3c84d0cb7156de20
KarlaJulyana <karla@solarz.com.br>
2024-09-01 16:13:32 -0300

    chore: remove authentication

--- doctors/views.py
+++ doctors/views.py
@@ -4,16 +4,9 @@ from rest_framework.response import Response
 from .models import Doctor
 from .serializers import DoctorSerializer
 
-class CustomDoctorPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.method == 'POST':
-            return True
-        return request.user and request.user.is_authenticated
-
 class DoctorViewSet(viewsets.ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
-    permission_classes = [CustomDoctorPermission]
     
     def partial_update(self, request, *args, **kwargs):
         return Response({'message': 'PATCH method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
