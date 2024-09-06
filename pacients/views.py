fc1ba9b4ac61ee0bb54010e785a6e6373b0e01f0
KarlaJulyana <karla@solarz.com.br>
2024-09-05 23:14:42 -0300

    fix: remove authentication from patient views

--- pacients/views.py
+++ pacients/views.py
@@ -1,19 +1,26 @@
+from rest_framework.decorators import action
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import viewsets, status, permissions
 from .serializers import PacientSerializer
 from pacients.models import Pacient
 
-class CustomPacientPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.method == 'POST': 
-            return True
-        return request.user and request.user.is_authenticated
-
 class PacientViewSet(viewsets.ModelViewSet):
     queryset = Pacient.objects.all()
     serializer_class = PacientSerializer
-    permission_classes = [CustomPacientPermission]
 
     def destroy(self, request, *args, **kwargs):
         return Response({"message": "Delete method not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+    @action(detail=False, methods=['post'], url_path='search-by-cpf')
+    def search_by_cpf(self, request):
+        cpf = request.data.get('cpf')
+        if cpf is None:
+            return Response({"error": "CPF is required"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            pacient = Pacient.objects.get(cpf=cpf)
+            serializer = self.get_serializer(pacient)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Pacient.DoesNotExist:
+            return Response({"error": "Pacient not found"}, status=status.HTTP_404_NOT_FOUND)
