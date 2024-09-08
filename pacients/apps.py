ef71d0ab6b2dd0878d4d33a9014f8cc7bbc1b8c6
KarlaJulyana <karla@solarz.com.br>
2024-09-08 20:16:20 -0300

    chore: add signals to patient

--- pacients/apps.py
+++ pacients/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PacientsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pacients'
+    
+    def ready(self):
+        import pacients.signals
---
+++ pacients/signals.py
@@ -0,0 +1,40 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Pacient
+import requests
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+@receiver(post_save, sender=Pacient)  # Certifique-se de substituir 'Patient' pelo nome correto do seu modelo
+def sync_patient_to_gateway_database(sender, instance, created, **kwargs):
+    if created:
+        user = instance.user
+
+        # Acessando a senha em texto plano armazenada temporariamente
+        plain_password = user.plain_password
+        
+        if plain_password is None:
+            logger.error("Plain password not found for user %s", user.email)
+            return
+
+        user_data = {
+            "email": user.email,
+            "password": plain_password,  # Senha em texto plano
+            "linked_user": user.id,
+        }
+        
+        logger.info("User data to sync: %s", user_data)
+
+        api_gateway_url = "http://localhost:8004/api/create-user/"  # URL do seu API Gateway
+        
+        try:
+            response = requests.post(api_gateway_url, json=user_data)
+            response.raise_for_status()
+            logger.info("Successfully synchronized with API Gateway: %s", response.status_code)
+        except requests.exceptions.RequestException as e:
+            logger.error("Error synchronizing patient with API Gateway: %s", e)
+            
+        # Removendo a senha em texto plano após enviar para o gateway
+        user.plain_password = None
